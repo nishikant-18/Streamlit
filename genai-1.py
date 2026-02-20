adb50733dcc840967f68f4f151893ad56d989aa1
nishikant-18 <nishikantw8999@gmail.com>
2026-02-20 16:30:18 +0530

    Add files via upload

---
+++ genai-1.py
@@ -0,0 +1,40 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+
+st.title("My First Streamlit App")
+st.write(":100: Hello Nishikant")
+st.text("Lets start")
+
+
+name=st.text_input("Enter Name:")
+if st.button("Greet"):
+    st.success(f"Hello, {name}")
+    
+    
+# creating charts using pandas and Numpy
+df=pd.DataFrame(np.random.randn(10,2),columns=['A','B'])
+
+st.line_chart(df)
+st.bar_chart(df)
+
+# sideBar, Image & video
+st.sidebar.title("Navigation")
+st.image(r"C:\Users\ADMIN\Downloads\photo.jpg",caption="sunset")
+st.video("https://youtu.be/NM22NLtnnko?si=s4mO-ZzwLXz9BO1-")
+
+#upload csv file
+upload_csv=st.file_uploader("upload a csv file", type="csv")
+
+if upload_csv:
+    df=pd.read_csv(upload_csv)
+    st.dataframe(df)
+    
+    # Text & Markdown Formating 
+    st.header("This is a Header")
+    st.subheader("this is a subheader")
+    
+    st.markdown("**Bold**, *Itlatic*, 'code' [Link](http://streamlit.io)")
+    st.code("for i in range(s): print(i)", language="python")
+
+
